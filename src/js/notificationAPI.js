e522b082a3c3d2f3f73041eada86c6d28a216a3e
Andreas-206 <puma_real@ukr.net>
2024-01-25 20:46:30 +0200

    img footer

--- src/js/notificationAPI.js
+++ src/js/notificationAPI.js
@@ -1,4 +1,10 @@
 import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css"
+
+export const messageObj = {
+  
+}
+
 
 const showMessage = (message, type = "info") => {
   iziToast[type]({
