ebfd72b204bf48ec1538eb5d33be41c98b67cebe
Antony Ishchenko <antony.ishchenko@gmail.com>
2024-01-28 12:13:13 +0200

    finish

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,16 +1,36 @@
-const scrollButton = document.querySelector(".scroll-button")
-const trackScroll = () => {
-    if (pageYOffset > 1000) {
-        scrollButton.classList.remove("is-hidden");
-    } else {
-        scrollButton.classList.add("is-hidden");
-    }
+const scrollButton = document.querySelector('.scroll-button');
+const scrollMiniIcon = document.querySelector('.scroll-mini');
+const scrollBigIcon = document.querySelector('.scroll-big');
+
+setScrollIcon();
+
+scrollButton.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
+
+window.addEventListener('scroll', () => {
+  document.documentElement.scrollTop >= 300 &&
+    scrollButton.classList.contains('is-hidden') &&
+    scrollButton.classList.remove('is-hidden');
+
+  document.documentElement.scrollTop < 300 &&
+    !scrollButton.classList.contains('is-hidden') &&
+    scrollButton.classList.add('is-hidden');
+});
+
+window.addEventListener('resize', () => {
+  setScrollIcon();
+});
+
+function setScrollIcon() {
+  if (document.documentElement.scrollWidth >= 1440) {
+    scrollBigIcon.classList.remove('is-undisplayed');
+    scrollMiniIcon.classList.add('is-undisplayed');
+  } else {
+    scrollBigIcon.classList.add('is-undisplayed');
+    scrollMiniIcon.classList.remove('is-undisplayed');
+  }
 }
-const goTop = () => {
-    scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    })
-}
-scrollButton.addEventListener("click", goTop);
-window.addEventListener("scroll", trackScroll);
