3136361ea60917f87e2b5276ae299e07807ff1c3
Oleguzik <guzik.oleg@gmail.com>
2024-01-28 18:33:25 +0100

    exercises initial commit

---
+++ src/js/excercises.js
@@ -0,0 +1,21 @@
+const exercisesType = document.querySelector('.exercises-type-item-btn');
+const exercisesGalleryType = document.querySelector('.exercises-gallery-list');
+// const typeBtn = []
+
+document.addEventListener("DOMContentLoaded", () => {
+  // alert("Page is loaded!");
+  exercisesType.classList.add('exercises-type-item-btn-focus');
+
+  console.log(exercisesType.);
+});
+
+exercisesType.addEventListener('click', () => {
+  exercisesType.classList.remove('exercises-type-item-btn-focus');
+  // alert("Page is loaded!");
+});
+
+
+
+// function selectTypeExercises() {
+//   console.log('11111111');  
+// };
