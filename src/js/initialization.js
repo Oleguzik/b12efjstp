538069fb643b2901f7b238947411f8666c4d43d3
NIKOLAEVTOLIA <140529562+NIKOLAEVTOLIA@users.noreply.github.com>
2024-01-28 02:54:10 +0200

    upd fquote\fadvice

--- src/js/initialization.js
+++ src/js/initialization.js
@@ -4,6 +4,6 @@ import localStorageAPI from './localStorageAPI';
 (async () => {
   let quoteData = await localStorageAPI.getQuoteOfTheDay();
   console.log(quoteData);
-  // document.querySelector('').textContent = quoteData.author;
-  // document.querySelector('').textContent = quoteData.quote;
+  document.querySelector('.quote-author').textContent = quoteData.author;
+  document.querySelector('.quote-text').textContent = quoteData.quote;
 })();
