8570cd4890ff1b980c06e886613f6415fb6539dd
[TatianaHolubenko] <tatiana.holubenko01@gmail.com>
2024-01-30 00:17:32 +0200

    all
    доробила все, але є деякі моменти які хочу уточнити

--- src/main.js
+++ src/main.js
@@ -22,95 +22,81 @@ import './js/initialization';
 //   // alert("Page is loaded!");
 // });
 
-
 // пришлось закоментить код выше, не работал мой код
 // код для скрытия кнопки очищения формы и показа ее, когда происходит ввод в поле импута
-  const inputElement = document.querySelector('.exercises-form-input');
-  const clearButton = document.querySelector('.exercises-form-btn-clear');
+const inputElement = document.querySelector('.exercises-form-input');
+const clearButton = document.querySelector('.exercises-form-btn-clear');
 
-
-  inputElement.addEventListener('input', function () {
-    const inputValue = inputElement.value;
-    if (inputValue.length > 0) {
-      clearButton.style.display = 'block';
-    } else {
-      clearButton.style.display = 'none';
-    }
-  });
-
-  clearButton.addEventListener('click', function () {
-    inputElement.value = '';
+inputElement.addEventListener('input', function () {
+  const inputValue = inputElement.value;
+  if (inputValue.length > 0) {
+    clearButton.style.display = 'block';
+  } else {
     clearButton.style.display = 'none';
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-/////////////////////////////////////////////////////////////////////
-////// modal-rating
-document.addEventListener('DOMContentLoaded', function () {
-  const ratingStars = document.querySelectorAll('.feedback-rating-stars-svg');
-  const ratingValue = document.querySelector('.feedback-form-rating-value');
-  let currentRating = 0;
-
-  ratingStars.forEach(star => {
-    star.addEventListener('mouseenter', function () {
-      const hoverStarIndex = Array.from(ratingStars).indexOf(star);
-      highlightStars(hoverStarIndex);
-
-      showStarValue(hoverStarIndex + 1);
-    });
-
-    star.addEventListener('mouseleave', function () {
-      if (currentRating === 0) {
-        resetRating();
-      } else {
-        highlightStars(currentRating - 1);
-        showStarValue(currentRating);
-      }
-    });
-    star.addEventListener('click', function () {
-      const clickStarIndex = Array.from(ratingStars).indexOf(star) + 1;
-      currentRating = clickStarIndex;
-      ratingValue.textContent = `${currentRating}.0`;
-
-      highlightStars(clickStarIndex - 1);
-    });
-  });
-
-  function highlightStars(index) {
-    ratingStars.forEach((star, i) => {
-      if (i <= index) {
-        star.classList.add('feedback-rating-stars-svg-highlight');
-      } else {
-        star.classList.remove('feedback-rating-stars-svg-highlight');
-      }
-    });
-  }
-
-  function resetRating() {
-    ratingStars.forEach(star => {
-      star.classList.remove('feedback-rating-stars-svg-highlight');
-    });
-    ratingValue.textContent = '0.0';
-  }
-
-  function showStarValue(value) {
-    ratingValue.textContent = `${value}.0`;
   }
 });
 
+clearButton.addEventListener('click', function () {
+  inputElement.value = '';
+  clearButton.style.display = 'none';
+});
+
+/////////////////////////////////////////////////////////////////////
+////// modal-rating
+// document.addEventListener('DOMContentLoaded', () => {
+//   const ratingStars = document.querySelectorAll('.feedback-rating-stars-svg');
+//   const ratingValue = document.querySelector('.feedback-form-rating-value');
+//   let currentRating = 0;
+
+//   ratingStars.forEach(star => {
+//     star.addEventListener('mouseenter', () => {
+//       const hoverStarIndex = Array.from(ratingStars).indexOf(star);
+//       highlightStars(hoverStarIndex);
+
+//       showStarValue(hoverStarIndex + 1);
+//     });
+
+//     star.addEventListener('mouseleave', () => {
+//       if (currentRating === 0) {
+//         resetRating();
+//       } else {
+//         highlightStars(currentRating - 1);
+//         showStarValue(currentRating);
+//       }
+//     });
+//     star.addEventListener('click', () => {
+//       const clickStarIndex = Array.from(ratingStars).indexOf(star) + 1;
+//       currentRating = clickStarIndex;
+//       ratingValue.textContent = `${currentRating}.0`;
+
+//       highlightStars(clickStarIndex - 1);
+//     });
+//   });
+
+//   function highlightStars(index) {
+//     ratingStars.forEach((star, i) => {
+//       if (i <= index) {
+//         star.classList.add('feedback-rating-stars-svg-highlight');
+//       } else {
+//         star.classList.remove('feedback-rating-stars-svg-highlight');
+//       }
+//     });
+//   }
+
+//   function resetRating() {
+//     ratingStars.forEach(star => {
+//       star.classList.remove('feedback-rating-stars-svg-highlight');
+//     });
+//     ratingValue.textContent = '0.0';
+//   }
+
+//   function showStarValue(value) {
+//     ratingValue.textContent = `${value}.0`;
+//   }
+// });
+
 ////// modal-rating
 /////////////////////////////////////////////////////////////////////
-
 
 // Останній (робочий) тест backendAPI лишаю закоментованим для прикладу запиту
 // async function GetAllData() {
@@ -146,7 +132,6 @@ document.addEventListener('DOMContentLoaded', function () {
 // console.log(ExerciseRating);
 // }
 // GetAllData();
-
 
 // const quote = localStorageAPI.getQuoteOfTheDay();
 // console.log(quote);
